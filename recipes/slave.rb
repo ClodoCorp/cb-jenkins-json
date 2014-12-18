54d237e66a2e478641efbc606c5f425f03974bcd
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-12-18 17:30:16 +0300

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -3,12 +3,30 @@ url = node['jenkins']['http_proxy']['host_name']
 username = node['jenkins']['http_proxy']['basic_auth_username']
 password = node['jenkins']['http_proxy']['basic_auth_password']
 
+ruby_block "jenkins-json waiting server" do
+  block do
+    require 'jenkins_api_client'
+    @client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}", :follow_redirects => true)
+    while true
+      begin
+        nodes = @client.node.list()
+        if nodes.length() > 0
+          break
+        end
+      rescue
+        Chef::Log.info ("jenkins-json waiting for master")
+        sleep 3
+      end
+    end
+  end
+end
+
 node['jenkins-json']['slave'].each do |name, options|
   ruby_block "jenkins-json node #{name}" do
     block do
       require 'jenkins_api_client'
       @client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}", :follow_redirects => true)
-      
+
       if name == 'master'
         conf = @client.node.get_config(name)
         conf = conf.sub(/<numExecutors>([0-9]+)<\/numExecutors>/, "<numExecutors>#{options['executors']}</numExecutors>")
@@ -20,7 +38,7 @@ node['jenkins-json']['slave'].each do |name, options|
       rescue
         Chef::Log.info ("jenkins-json unable to delete node #{name}")
       end
-      
+
       while @client.node.index(name).instance_of? Array
         @client.node.create_dumb_slave(name: name, slave_host: options['slave_host'],
                                        slave_port: options['slave_port'], slave_user: options['slave_user'],
@@ -29,28 +47,8 @@ node['jenkins-json']['slave'].each do |name, options|
                                        executors: options['executors'], labels: options['labels'],
                                        remote_fs: options['remote_fs'], mode: options['mode'])
         Chef::Log.info ("jenkins-json waiting for slave #{name}")
-        sleep 1
+        sleep 3
       end
     end
   end
 end
-
-ruby_block "jenkins-json reload server" do
-  block do
-    require 'jenkins_api_client'
-    @client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}", :follow_redirects => true)
-    @client.system.wait_for_ready
-    @client.system.reload
-    while true
-      begin
-        @client.system.wait_for_ready
-        break
-      rescue
-        Chef::Log.info ("jenkins-json waiting for master")
-        sleep 1
-        next
-      end
-    end
-  end
-end
-
