fe9c7c9d81fd4628b6782071dd557936299c6415
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-12-18 03:40:34 +0300

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -1,18 +1,31 @@
 
-require 'jenkins_api_client'
-
 url = node['jenkins']['http_proxy']['host_name']
 username = node['jenkins']['http_proxy']['basic_auth_username']
 password = node['jenkins']['http_proxy']['basic_auth_password']
 
-@client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}")
+node['jenkins-json']['slave'].each do |slave|
+  slave.each do |name, options|
+    ruby_block "jenkins-json node #{name}" do
+      block do
+        require 'jenkins_api_client'
+        @client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}", :follow_redirects => true)
 
-node['jenkins-json']['slave'].each do |name, options|
-  @client.node.create_dumb_slave(
-                                 name: name,
-                                 slave_host: options['slave_host'],
-                                 private_key_file: options['/root/.ssh/id_rsa'],
-                                 executors: options['executors'],
-                                 labels: options['labels']
-                                 )
+        begin
+          @client.node.delete(name)
+        rescue
+          Chef::Log.info ("jenkins-json unable to delete node #{name}")
+        end
+
+        while @client.node.index(name).instance_of? Array
+          @client.node.create_dumb_slave(name: name, slave_host: options['slave_host'],
+                                         private_key_file: options['private_key_file'],
+                                         executors: options['executors'], labels: options['labels'],
+                                         remote_fs: options['remote_fs'])
+          Chef::Log.info ("jenkins-json waiting for slave #{name}")
+          sleep 1
+        end
+
+      end
+    end
+  end
 end
