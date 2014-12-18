ba9af55b8093ef79d6aa2942ac20ddc8df7f3ec6
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-12-18 16:44:29 +0300

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -3,38 +3,36 @@ url = node['jenkins']['http_proxy']['host_name']
 username = node['jenkins']['http_proxy']['basic_auth_username']
 password = node['jenkins']['http_proxy']['basic_auth_password']
 
-node['jenkins-json']['slave'].each do |slave|
-  slave.each do |name, options|
-    ruby_block "jenkins-json node #{name}" do
-      block do
-        require 'jenkins_api_client'
-        @client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}", :follow_redirects => true)
-
-        if name == 'master'
-          conf = @client.node.get_config(name)
-          conf = conf.sub(/<numExecutors>([0-9]+)<\/numExecutors>/, "<numExecutors>#{options['executors']}</numExecutors>")
-          @client.node.post_config(name, conf)
-          next
-        end
-        begin
-          @client.node.delete(name)
-        rescue
-          Chef::Log.info ("jenkins-json unable to delete node #{name}")
-        end
-
-        while @client.node.index(name).instance_of? Array
-          @client.node.create_dumb_slave(name: name, slave_host: options['slave_host'],
-                                         slave_port: options['slave_port'], slave_user: options['slave_user'],
-                                         credentials_id: options['credentials_id'],
-                                         private_key_file: options['private_key_file'],
-                                         executors: options['executors'], labels: options['labels'],
-                                         remote_fs: options['remote_fs'], mode: options['mode'])
-          Chef::Log.info ("jenkins-json waiting for slave #{name}")
-          sleep 1
-        end
-
+node['jenkins-json']['slave'].each do |name, options|
+  ruby_block "jenkins-json node #{name}" do
+    block do
+      require 'jenkins_api_client'
+      @client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}", :follow_redirects => true)
+      
+      if name == 'master'
+        conf = @client.node.get_config(name)
+        conf = conf.sub(/<numExecutors>([0-9]+)<\/numExecutors>/, "<numExecutors>#{options['executors']}</numExecutors>")
+        @client.node.post_config(name, conf)
+        next
+      end
+      begin
+        @client.node.delete(name)
+      rescue
+        Chef::Log.info ("jenkins-json unable to delete node #{name}")
+      end
+      
+      while @client.node.index(name).instance_of? Array
+        @client.node.create_dumb_slave(name: name, slave_host: options['slave_host'],
+                                       slave_port: options['slave_port'], slave_user: options['slave_user'],
+                                       credentials_id: options['credentials_id'],
+                                       private_key_file: options['private_key_file'],
+                                       executors: options['executors'], labels: options['labels'],
+                                       remote_fs: options['remote_fs'], mode: options['mode'])
+        Chef::Log.info ("jenkins-json waiting for slave #{name}")
+        sleep 1
       end
     end
   end
 end
+
 
