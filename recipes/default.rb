5c3a04591357fc39b8d56514f141d9d3a3afaa58
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-12-25 19:30:55 +0300

    cleanup jobs
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,4 @@
 chef_gem 'jenkins_api_client'
 
 include_recipe 'jenkins-json::slave' unless node['jenkins-json']['slave'].nil?
+include_recipe 'jenkins-json::jobs' unless node['jenkins-json']['jobs'].nil?
---
+++ recipes/jobs.rb
@@ -0,0 +1,42 @@
+
+url = node['jenkins']['http_proxy']['host_name']
+username = node['jenkins']['http_proxy']['basic_auth_username']
+password = node['jenkins']['http_proxy']['basic_auth_password']
+
+unless node['jenkins-json']['jobs']['prune'].nil?
+
+  ruby_block "prune deleted jobs" do
+    ruby_block "create_slave_#{name}" do
+      block do
+        require 'jenkins_api_client'
+        @client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}", :follow_redirects => true)
+        
+        while true
+          begin
+            nodes = @client.node.list()
+            if nodes.length() > 0
+              break
+            end
+          rescue
+            Chef::Log.info ("jenkins-json waiting for master")
+            sleep 3
+          end
+        end
+        
+        sjobs = @client.job.list_all()
+        djobs = search(node["jenkins"]["jobs"]["databag"], "*:*")
+        Chef::Log.info ("DDD #{djobs}")
+#        pjobs = sjobs - djobs
+
+#        pjobs.each do |job|
+#          Chef::Log.info ("jenkins-json wipe workspace for job: #{job}")
+#          @client.job.wipe_out_workspace(job)
+#          Chef::Log.info ("jenkins-json delete job: #{job}")
+#          @client.job.delete(job)
+#        end
+        
+      end
+    end
+  end
+end
+
