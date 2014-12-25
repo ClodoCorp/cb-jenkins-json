4fb8dd95f6aa495cc2ff7f054dfc0874ca219340
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-12-25 19:33:41 +0300

    cleanup jobs
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- recipes/jobs.rb
+++ recipes/jobs.rb
@@ -6,36 +6,34 @@ password = node['jenkins']['http_proxy']['basic_auth_password']
 if node['jenkins-json']['jobs']['prune']
 
   ruby_block "prune deleted jobs" do
-    ruby_block "create_slave_#{name}" do
-      block do
-        require 'jenkins_api_client'
-        @client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}", :follow_redirects => true)
-        
-        while true
-          begin
-            nodes = @client.node.list()
-            if nodes.length() > 0
-              break
-            end
-          rescue
-            Chef::Log.info ("jenkins-json waiting for master")
-            sleep 3
+    block do
+      require 'jenkins_api_client'
+      @client = JenkinsApi::Client.new(server_url: "http://#{username}:#{password}@#{url}", :follow_redirects => true)
+      
+      while true
+        begin
+          nodes = @client.node.list()
+          if nodes.length() > 0
+            break
           end
+        rescue
+          Chef::Log.info ("jenkins-json waiting for master")
+            sleep 3
         end
-        
-        sjobs = @client.job.list_all()
-        djobs = search(node["jenkins"]["jobs"]["databag"], "*:*")
-        Chef::Log.info ("DDD #{djobs}")
-#        pjobs = sjobs - djobs
-
-#        pjobs.each do |job|
-#          Chef::Log.info ("jenkins-json wipe workspace for job: #{job}")
-#          @client.job.wipe_out_workspace(job)
-#          Chef::Log.info ("jenkins-json delete job: #{job}")
-#          @client.job.delete(job)
-#        end
-        
       end
+      
+      sjobs = @client.job.list_all()
+      djobs = search(node["jenkins"]["jobs"]["databag"], "*:*")
+      Chef::Log.info ("DDD #{djobs}")
+      #        pjobs = sjobs - djobs
+      
+      #        pjobs.each do |job|
+      #          Chef::Log.info ("jenkins-json wipe workspace for job: #{job}")
+      #          @client.job.wipe_out_workspace(job)
+      #          Chef::Log.info ("jenkins-json delete job: #{job}")
+      #          @client.job.delete(job)
+      #        end
+      
     end
   end
 end
